12e64f1273b204fe15ffa95565f7da0efd920285
fuhrmann.software <fuhrmann@fuhrmann.software>
2024-04-10 21:17:40 +0300

    no parking mobile image

--- src/components/noParking/noParking.jsx
+++ src/components/noParking/noParking.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import { OpenAppButton } from '@/components/buttons/openAppButton';
 import NoParkingImage from '@/images/global/no-parking.png';
+import NoParkingMobileImage from '@/images/global/no-parking-mobile.png';
 import { CornerButtonContainer } from '@/components/buttons/cornerButtonContainer';
 import { RoundButton } from '@/components/buttons/roundButton';
 import { ArrowDown } from '@/components/icons/arrowDown';
@@ -12,19 +13,25 @@ export const NoParking = () => {
   return (
     <div className='relative h-[700px] w-full overflow-hidden rounded-bl-card rounded-tl-card  rounded-tr-card max-md:h-[580px]'>
       <div className='absolute left-0 z-[1] p-16'>
-        <div className='mb-16 w-2/3 text-7xl leading-[1.2] text-red-600 max-md:w-full max-md:text-3xl'>
+        <div className='mb-16 w-2/3 text-7xl leading-[1.2] text-red-600 max-md:w-full max-md:text-3xl max-lg:text-5xl max-lg:mb-12 max-md:mb-6'>
           <span className='text-white'>Need to fine wrong parker?</span>
           <br />
           <span className='text-white/50'>We made it easy.</span>
         </div>
         <OpenAppButton />
       </div>
-      <Image
-        src={NoParkingImage}
-        fill
-        alt='No Parking'
-        className='object-cover object-right max-sm:object-center'
-      />
+        <Image
+            src={NoParkingImage}
+            fill
+            alt='No Parking'
+            className='object-cover object-right-bottom max-sm:hidden'
+        />
+        <Image
+            src={NoParkingMobileImage}
+            fill
+            alt='No Parking'
+            className='object-cover object-right-bottom sm:hidden'
+        />
       <CornerButtonContainer>
         <RoundButton
           icon={ArrowDown}
